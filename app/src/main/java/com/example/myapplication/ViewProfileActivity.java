fde36b89ebcda9ffecdda625710b0abcf3e823df
claudiu.cocan <cocanclaudiu18@stud.ase.ro>
2022-07-04 09:58:29 +0300

    viewprofile cu change email

--- app/src/main/java/com/example/myapplication/ViewProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ViewProfileActivity.java
@@ -4,27 +4,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ViewProfileActivity extends AppCompatActivity {
 
-
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
 
+
         MaterialButtonToggleGroup btnGroup3;
-        Button btnHome3,btnBookings3, btnProfile3;
+        Button btnHome3,btnBookings3, btnProfile3, butonDelogare, butonSchimbaEmail, butonSchimbaParola;
+        EditText editParolaProfil, editEmailProfil;
+        TextView textEmailProfil;
+
 
         btnGroup3 = findViewById(R.id.btnGroup3);
         btnHome3 = findViewById(R.id.btnHome3);
         btnBookings3 = findViewById(R.id.btnBookings3);
         btnProfile3 = findViewById(R.id.btnProfile3);
+        butonDelogare = findViewById(R.id.butonDelogare);
+        butonSchimbaEmail = findViewById(R.id.butonSchimbaEmail);
+        butonSchimbaParola = findViewById(R.id.butonSchimbaParola);
+        textEmailProfil = findViewById(R.id.textEmailProfil);
+        editParolaProfil = findViewById(R.id.editParolaProfil);
+        editEmailProfil = findViewById(R.id.editEmailProfil);
 
+        textEmailProfil.setText(mAuth.getCurrentUser().getEmail());
+
+        butonSchimbaEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if( !editEmailProfil.getText().toString().isEmpty()) {
+                    /*if (editEmailProfil.getText().toString().contains("@")
+                            && !editEmailProfil.getText().toString().contains(".com")
+                            && (!editEmailProfil.getText().toString().contains("yahoo")
+                            || !editEmailProfil.getText().toString().contains("gmail"))) {*/
+
+                        mAuth.getCurrentUser().updateEmail(editEmailProfil.getText().toString());
+                        textEmailProfil.setText(mAuth.getCurrentUser().getEmail());
+                        //textEmailProfil.refreshDrawableState();
+                        Toast.makeText(ViewProfileActivity.this, "Adresa de e-mail a fost schimbată", Toast.LENGTH_SHORT).show();
+                        finish();
+                        overridePendingTransition(0, 0);
+                        startActivity(getIntent());
+                        overridePendingTransition(0, 0);
+                    /*}*/
+                }
+                else {
+                    Toast.makeText(ViewProfileActivity.this, "Introduceți noua adresă de e-mail", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         btnGroup3.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
             @Override
